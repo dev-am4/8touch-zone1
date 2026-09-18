9e5849420049c84e9b4b4bc9202bd7cb41799cf1
dev-am4 <am4.production29@gmail.com>
2026-09-18 14:42:02 +0700

    Expose kiosk media inventory to renderer

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,6 +1,8 @@
-const { contextBridge } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('zone1Kiosk', {
   platform: process.platform,
   isElectron: true,
+  mediaProtocol: 'zone1-media',
+  getMediaStatus: () => ipcRenderer.invoke('zone1:get-media-status'),
 })
